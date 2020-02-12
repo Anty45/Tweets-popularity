e29488745635bd1c22abfbb7242c9766dceb6364
akoffi <anty-frederic.koffi@etudiant.univ-rennes1.fr>
2020-02-12 21:46:22 +0100

    python version of kafka bus. Easy way to switch to machine learning task

---
+++ consummer.py
@@ -0,0 +1,36 @@
+from kafka import KafkaConsumer
+from pymongo import MongoClient
+import json
+from json import loads
+
+if __name__ == '__main__':
+
+    consumer = KafkaConsumer(
+        'test',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='my-group',
+        value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+    # Connexion à une collection mongo appellée Twitter
+
+    try:
+        client = MongoClient()
+        db = client['Twitter']
+        print("Connected")
+        collection = db["Doncic"]
+    except:
+        print("Unable to connect to Mongo")
+
+    # Consommation des messages
+
+    for message in consumer:
+        print("ok")
+        try:
+            message = message.value
+            result = collection.insert_one(message)
+            print('{} added to {}'.format(message, collection))
+            print(result)
+        except:
+            print("Can not write in Mongo.")
---
+++ run.py
@@ -0,0 +1,55 @@
+import tweepy
+import time
+from kafka import KafkaConsumer, KafkaProducer
+from datetime import datetime, timedelta
+from json import dumps
+
+if __name__ == '__main__':
+
+    # twitter setup
+    consumer_key = "kc17v5bzrUypcf6zH2pr3wegZ"
+    consumer_secret = "4yhDcoBvtG5FEOG0y2ETfozAnq6qL3orn0RACdIjAzAZe4DAO1"
+    access_token = "3395703293-clKkQY8wxu0MOunjRpzT9p4ZQv7Q3zCHTUO1g7s"
+    access_token_secret = "bw3zH1XTqO9dSPTzBIN5M7qlqOQO78urcdOGZ05Y3blfk"
+    # Creating the authentication object
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    # Setting your access token and secret
+    auth.set_access_token(access_token, access_token_secret)
+    # Creating the API object by passing in auth information
+    api = tweepy.API(auth)
+
+    def normalize_timestamp(time):
+        mytime = datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+        mytime += timedelta(hours=1)  # the tweets are timestamped in GMT timezone, while I am in +1 timezone
+        return (mytime.strftime("%Y-%m-%d %H:%M:%S"))
+
+    # Setup producer by rooting it to our bootstrap_servers
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                             value_serializer=lambda v: dumps(v).encode('utf-8'))  # transformation en json avant de le convertir en utf8)
+    topic_name = 'test'
+
+    # Script to get Data related to NBA
+    def get_twitter_data():
+        res = api.search("Doncic",lang = "fr")
+        for i in res:
+
+            # producer.send(topic_name, str.encode(record))
+            producer.send(topic_name, value={'user_id': str(i.user.id_str),
+                                             "texte": str(i.text),
+                                             'created_at': str(normalize_timestamp(str(i.created_at))),
+                                             'followers_count': str(i.user.followers_count),
+                                             'location': str(i.user.location),
+                                             'fav': str(i.favorite_count),
+                                             'retweet': str(i.retweet_count)})
+            #producer.send(topic_name, value={"texte": i.text})
+
+    # Get Data
+    get_twitter_data()
+
+    def periodic_work(interval):
+        while True:
+            get_twitter_data()
+            # interval should be an integer, the number of seconds to wait
+            time.sleep(interval)
+
+    periodic_work(60 * 0.1)  # get data every couple of minutes
