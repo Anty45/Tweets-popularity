12b6bd99b6ea1f75510bdcfcb618533efadcb20c
Anty45 <anty.koffi@inphb.ci>
2021-07-14 17:24:29 +0200

    random tweets find by top trend anchor

--- Kafka-twitter-connect/run.py
+++ Kafka-twitter-connect/run.py
@@ -3,6 +3,8 @@ import time
 from kafka import KafkaConsumer, KafkaProducer
 from datetime import datetime, timedelta
 from json import dumps
+import secrets
+from typing import List
 
 if __name__ == '__main__':
 
@@ -16,41 +18,59 @@ if __name__ == '__main__':
     # Setting your access token and secret
     auth.set_access_token(access_token, access_token_secret)
     # Creating the API object by passing in auth information
-    api = tweepy.API(auth,wait_on_rate_limit=True)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    _MAX_TWEETS_TO_FETCH_ = 50
+    _WOEID_FRANCE_ = '23424819'
+    topic_name = 'test'
 
     def normalize_timestamp(time):
         mytime = datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
         mytime += timedelta(hours=1)  # the tweets are timestamped in GMT timezone, while I am in +1 timezone
-        return (mytime.strftime("%Y-%m-%d %H:%M:%S"))
+        return mytime.strftime("%Y-%m-%d %H:%M:%S")
 
     # Setup producer by rooting it to our bootstrap_servers
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                             value_serializer=lambda v: dumps(v).encode('utf-8'))  # transformation en json avant de le convertir en utf8)
-    topic_name = 'test'
+                             value_serializer=lambda v: dumps(v).encode(
+                                 'utf-8'))  # transformation en json avant de le convertir en utf8)
 
-    # Script to get Data related to NBA
-    def get_twitter_data():
-        res = api.search("Federer",lang = "en")
+    def get_trends_by_woeid(woeid: str) -> List[dict]:
+        result_request = api.trends_place(id= woeid)
+        print(f"Trends from {woeid} fetched...")
+
+        return result_request
+
+    def find_tops_trend_name(trends: List[dict]) -> List[str]:
+        trends_attributes = trends[0]["trends"]
+        tops_trend_names = [trend_attribute["name"] for trend_attribute in trends_attributes]
+        return tops_trend_names
+
+    def get_twitter_data(max_tweet_to_fetch: int, trends: List[dict]) -> None:
+        trends_names = find_tops_trend_name(trends)
+        random_top_trend_name = secrets.choice(trends_names)
+        res = api.search(q=random_top_trend_name,
+                         lang="fr",
+                         result_type="mixed",
+                         count=max_tweet_to_fetch)
+        if len(res) != 0:
+            print("group of tweets fetched")
+
         for i in res:
-            # Excluce Retwwet :
-
             if (not i.retweeted) and ('RT @' not in i.text):
                 producer.send(topic_name, value={'user_id': str(i.user.id_str),
                                                  "texte": str(i.text),
                                                  'created_at': str(normalize_timestamp(str(i.created_at))),
                                                  'followers_count': str(i.user.followers_count),
                                                  'location': str(i.user.location),
-                                                 'lang' : str(i.lang),
+                                                 'lang': str(i.lang),
                                                  'fav': str(i.favorite_count),
                                                  'retweet': str(i.retweet_count)})
 
-    # Get Data
-    get_twitter_data()
-
-    def periodic_work(interval):
+    def periodic_work(interval: int):
         while True:
-            get_twitter_data()
+            french_trends = get_trends_by_woeid(woeid=_WOEID_FRANCE_)
+            get_twitter_data(max_tweet_to_fetch=_MAX_TWEETS_TO_FETCH_, trends = french_trends)
             # interval should be an integer, the number of seconds to wait
             time.sleep(interval)
 
-    periodic_work(60*15)  # get data every 15 minutes
+
+    periodic_work(60 * 15)  # get data every 15 minutes
