0073ace651f99fadad4dd9ff81d356170471f04e
Anty45 <anty.koffi@inphb.ci>
2022-04-07 18:12:14 +0200

    Evaluate model

--- scripts/Model/evaluate.py
+++ scripts/Model/evaluate.py
@@ -1,11 +1,39 @@
 import lightgbm
-from sklearn.metrics import roc_auc_score, f1_score
-import numpy as np
+import pandas as pd
+from sklearn.metrics import f1_score, classification_report
+from scripts.fetch_data.save_data import PATH_TO_TEST, PATH_TO_FAV_MODEL, _LABELS_
+import joblib
+
+from scripts.preprocessor.preprocessor import select_features_and_target, isolate_target
 
 
-def evaluate_model(model: lightgbm.LGBMClassifier, features, labels):
+def validate_model(model: lightgbm.LGBMClassifier, features, labels):
     predictions_raw = model.predict(X=features)
-    # predictions_classes = np.argmax(predictions_raw, axis=1)
-    scores = f1_score(y_true=labels, y_pred=predictions_raw,average="macro")
+    scores = f1_score(y_true=labels, y_pred=predictions_raw, average="macro")
 
     return scores
+
+
+def model_test(model: lightgbm.LGBMClassifier, test_features, test_labels):
+    predictions_test = model.predict(X=test_features)
+
+    return classification_report(y_true=test_labels, y_pred=predictions_test)
+
+
+if __name__ == "__main__":
+    model_ = joblib.load(filename=PATH_TO_FAV_MODEL)
+    test = pd.read_csv(PATH_TO_TEST, index_col=False)
+    test = select_features_and_target(dataframe=test)
+
+    x_test, y_test = isolate_target(test)
+
+    test_targets = [fav_rt[_LABELS_["_FAV_LABEL_"]] for fav_rt in y_test]
+
+    test_dataset = lightgbm.Dataset(data=x_test,
+                                    label=test_targets,
+                                    )
+    classif_reports = model_test(model=model_,
+                                 test_features=test_dataset.data,
+                                 test_labels=test_dataset.label
+                                 )
+    print(classif_reports)
--- scripts/fetch_data/save_data.py
+++ scripts/fetch_data/save_data.py
@@ -8,6 +8,15 @@ PATH_TO_TRAIN = Path(__file__) / "../../../ressources/app.csv"
 PATH_TO_VAL = Path(__file__) / "../../../ressources/val.csv"
 PATH_TO_TEST = Path(__file__) / "../../../ressources/test.csv"
 PATH_TO_PREPROCESSOR = Path(__file__) / "../../../ressources/preprocessor.joblib"
+PATH_TO_FAV_MODEL = Path(__file__) / "../../../ressources/model_fav.joblib"
+PATH_TO_RT_MODEL = Path(__file__) / "../../../ressources/model_fav.joblib"
+_LABELS_ = {"_FAV_LABEL_": 0,
+            "_RT_LABEL_": 1}
+
+TARGETS = {"low_level": 0,
+           "mid_level": 1,
+           "mid_high_level": 2,
+           "high_level": 3}
 
 
 def add_data_to_df(path_to_df: str, df_to_add: pd.DataFrame) -> None:
