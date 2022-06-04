cfb5071421d177a7da8eec5c809f615c0cfa46ab
Anty45 <anty.koffi@inphb.ci>
2022-06-04 20:54:02 +0200

    Expose twitter fav model api with FastAPI

--- scripts/api/twitter_model_api.py
+++ scripts/api/twitter_model_api.py
@@ -1,7 +1,30 @@
+from pathlib import Path
+import pandas as pd
 from fastapi import FastAPI
+import joblib
+from pydantic import BaseModel
+
+PATH_TO_FAV_MODEL = Path(__file__) / "../../../ressources/model_fav.joblib"
+
+
+class Features_twitter_fav(BaseModel):
+    tweet_quarter: int
+    tweet_day_of_the_week: int
+    tweet_hour: int
+    trend_volume: int
+    is_verified: int
+    followers_count: int
+
 
 app = FastAPI()
+model = joblib.load(filename=PATH_TO_FAV_MODEL)
 
-@app.get("/predict")
-async def root():
-    return {"popularity_score": 0}
+
+
+@app.post("/predict/")
+async def root(features: Features_twitter_fav):
+    features_parsed = features.dict()
+    df_feats = pd.DataFrame(features_parsed, index=[0])
+
+    popularity_predict = model.predict(X=df_feats.values)
+    return {"popularity_score": popularity_predict.tolist()}
